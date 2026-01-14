d656f4b42cf1d8b4414382e04bd4bf12ae12afc9
poojak2008 <poojabkamble20@gmail.com>
2026-01-14 18:51:11 +0530

    Commit

---
+++ CinemaGhar/Screens/Favourites/Views/FavoritesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoritesTableViewCell.swift
+//  CinemaGhar
+//
+//  Created by pooja kamble on 13/01/26.
+//
+
+import UIKit
+
+class FavoritesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CinemaGhar/Screens/Home/View/HeroPosterCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HeroPosterCollectionViewCell.swift
+//  CinemaGhar
+//
+//  Created by pooja kamble on 14/01/26.
+//
+
+import UIKit
+
+class HeroPosterCollectionViewCell: UICollectionViewCell {
+    
+}
